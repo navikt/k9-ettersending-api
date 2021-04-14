53b7bc36394133b8b8446fe6e4afa4b586134e91
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-04-14 08:28:17 +0200

    Erstattet jcenter med jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ repositories {
     }
 
     mavenCentral()
-    jcenter()
 
+    maven("https://jitpack.io")
     maven("https://dl.bintray.com/kotlin/ktor")
     maven("https://kotlin.bintray.com/kotlinx")
     maven("http://packages.confluent.io/maven/")
