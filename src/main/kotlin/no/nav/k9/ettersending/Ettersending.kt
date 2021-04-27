bc88a3e7ef0dd941890b951a711910fb78db23dc
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-04-27 08:54:38 +0200

    Fjerner deprecated variant som frontend ikke bruker lenger.

--- src/main/kotlin/no/nav/k9/ettersending/Ettersending.kt
+++ src/main/kotlin/no/nav/k9/ettersending/Ettersending.kt
@@ -1,6 +1,5 @@
 package no.nav.k9.ettersending
 
-import com.fasterxml.jackson.annotation.JsonAlias
 import java.net.URL
 import java.util.*
 
@@ -15,9 +14,7 @@ data class Ettersending(
 )
 
 enum class Søknadstype{
-    @JsonAlias("pleiepenger") //TODO 23.03.2021 - Alias for å støtte gammel versjon fra frontend
     PLEIEPENGER_SYKT_BARN,
-    @JsonAlias("omsorgspenger") //TODO 23.03.2021 - Alias for å støtte gammel versjon fra frontend
     OMP_UTV_KS, // Omsorgspenger utvidet rett - kronisk syke eller funksjonshemming.
     OMP_UT_SNF, // Omsorgspenger utbetaling SNF ytelse.
     OMP_UT_ARBEIDSTAKER, // Omsorgspenger utbetaling arbeidstaker ytelse.
