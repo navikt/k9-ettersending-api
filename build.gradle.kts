e86704cd3d6349afe33b89ef8ce4203807a24128
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-11-04 10:56:55 +0100

    Oppdaterer dusseldorfKtorVersion, awaitility-kotlin og actions/checkout. Samkjører Java versjon. Dytter opp til nyeste K9-format versjon. Ingen endringer som berører denne ytelsen.
    * Closes #67
    * Closes #68

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val mainClass = "no.nav.k9.AppKt"
-val dusseldorfKtorVersion = "2.1.6.2-6ce5eaa"
+val dusseldorfKtorVersion = "3.1.6.4-e07c5ec"
 val ktorVersion = ext.get("ktorVersion").toString()
-val k9FormatVersion = "5.4.25"
+val k9FormatVersion = "5.5.8"
 val fuelVersion = "2.3.1"
 val kafkaEmbeddedEnvVersion = ext.get("kafkaEmbeddedEnvVersion").toString()
 val kafkaVersion = ext.get("kafkaVersion").toString() // Alligned med version fra kafka-embedded-env
@@ -16,7 +16,7 @@ plugins {
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/6ce5eaa4666595bb6b550fca5ca8bbdc242961a0/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/e07c5ecf831928eb250c946e753aff2a3b798295/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
@@ -50,7 +50,7 @@ dependencies {
         exclude(group = "org.eclipse.jetty")
     }
     testImplementation ("org.skyscreamer:jsonassert:1.5.0")
-    testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+    testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
     testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
@@ -80,11 +80,11 @@ java {
 
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.withType<ShadowJar> {
