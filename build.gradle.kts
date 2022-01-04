5949174226e48bf788516250dc891fb311df9c50
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-01-04 11:50:42 +0100

    Oppdaterer til nyeste

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val mainClass = "no.nav.k9.AppKt"
-val dusseldorfKtorVersion = "3.1.6.7-c48e02f"
+val dusseldorfKtorVersion = "3.1.6.7-7d85d28"
 val ktorVersion = ext.get("ktorVersion").toString()
 val k9FormatVersion = "5.7.2"
 val fuelVersion = "2.3.1"
@@ -16,7 +16,7 @@ plugins {
 
 buildscript {
     // Henter ut diverse dependency versjoner, i.e. ktorVersion.
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/c48e02f6fc77e86e5e2db673d54708efa41819d7/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/7d85d28ba39e0331f3f33e614be4ded35be00273/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
